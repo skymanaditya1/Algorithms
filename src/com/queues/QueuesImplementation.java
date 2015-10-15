033101764971f706df6d25af540c0a9cebf571f7
skymanaditya1 <skymanaditya1@gmail.com>
2015-10-16 00:52:20 +0530

    Files corresponding to queues programs

---
+++ src/com/queues/QueuesImplementation.java
@@ -0,0 +1,13 @@
+package com.queues;
+
+import java.util.Scanner;
+
+/**
+ * Created by aditya on 16/10/15.
+ */
+
+public class QueuesImplementation {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+    }
+}
